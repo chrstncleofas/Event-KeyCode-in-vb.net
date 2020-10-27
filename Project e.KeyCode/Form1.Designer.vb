41f04d3fc92dff8fb711ebbfcdab0d5a83ed60c2
Christian <cleofastiano27@gmail.com>
2020-10-27 09:57:32 +0800

    Event KeyCode

---
+++ Project e.KeyCode/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblAve = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(37, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Input 01"
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNum1.Location = New System.Drawing.Point(42, 89)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(244, 33)
+        Me.txtNum1.TabIndex = 1
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNum2.Location = New System.Drawing.Point(42, 175)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(244, 33)
+        Me.txtNum2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(37, 136)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Input 01"
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.BackColor = System.Drawing.Color.White
+        Me.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAnswer.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer.Location = New System.Drawing.Point(42, 260)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(244, 33)
+        Me.lblAnswer.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(37, 223)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 25)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Result"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(40, 308)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 25)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Average"
+        '
+        'lblAve
+        '
+        Me.lblAve.BackColor = System.Drawing.Color.White
+        Me.lblAve.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAve.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAve.Location = New System.Drawing.Point(42, 344)
+        Me.lblAve.Name = "lblAve"
+        Me.lblAve.Size = New System.Drawing.Size(244, 33)
+        Me.lblAve.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(339, 430)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblAve)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtNum1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lblAve As System.Windows.Forms.Label
+
+End Class
---
+++ Project e.KeyCode/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Private Sub txtNum2_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtNum2.KeyDown
+        If e.KeyCode = Keys.F1 Then
+            lblAnswer.Text = Val(txtNum1.Text) + Val(txtNum2.Text)
+        ElseIf e.KeyCode = Keys.F12 Then
+            lblAnswer.Text = Val(txtNum1.Text) - Val(txtNum2.Text)
+        ElseIf e.KeyCode = Keys.Enter Then
+            lblAnswer.Text = Val(txtNum1.Text) * Val(txtNum2.Text)
+        ElseIf e.KeyCode = Keys.Escape Then
+            lblAnswer.Text = Val(txtNum1.Text) / Val(txtNum2.Text)
+            If txtNum2.Text = 0 Then
+                MsgBox("Cannot divide by Zero", vbCritical, "Error")
+                txtNum1.Clear()
+                txtNum1.Focus()
+                txtNum2.Clear()
+                lblAnswer.Text = ""
+                lblAve.Text = ""
+            End If
+        ElseIf e.KeyCode = Keys.Delete Then
+            lblAve.Text = Val(lblAnswer.Text) / 2
+        ElseIf e.KeyCode = Keys.Back Then
+            txtNum1.Clear()
+            txtNum1.Focus()
+            txtNum2.Clear()
+            lblAnswer.Text = ""
+            lblAve.Text = ""
+        ElseIf e.KeyCode = Keys.F2 Then
+            If MsgBox("Do you want to close this window?", vbQuestion + vbYesNo, "E.KeyCode") = vbYes Then
+                Me.Close()
+            Else
+                Me.Focus()
+            End If
+        End If
+    End Sub
+
+End Class
+
